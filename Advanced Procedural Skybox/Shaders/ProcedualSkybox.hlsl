9d9231a7fabdba32069c5cff9194a5145dd90689
BxKangKi <80934730+BxKangKi@users.noreply.github.com>
2023-09-29 17:19:14 +0900

    Add Moon

--- Advanced Procedural Skybox/Shaders/ProcedualSkybox.hlsl
+++ Advanced Procedural Skybox/Shaders/ProcedualSkybox.hlsl
@@ -20,32 +20,32 @@
 
 // RGB wavelengths
 // .35 (.62=158), .43 (.68=174), .525 (.75=190)
-static const half3 kDefaultScatteringWavelength = half3(.65, .57, .475);
-static const half3 kVariableRangeForScatteringWavelength = half3(.15, .15, .15);
+static const float3 kDefaultScatteringWavelength = float3(.65, .57, .475);
+static const float3 kVariableRangeForScatteringWavelength = float3(.15, .15, .15);
 
 #define OUTER_RADIUS 1.025
-static const half kOuterRadius = OUTER_RADIUS;
-static const half kOuterRadius2 = OUTER_RADIUS*OUTER_RADIUS;
-static const half kInnerRadius = 1.0;
-static const half kInnerRadius2 = 1.0;
+static const float kOuterRadius = OUTER_RADIUS;
+static const float kOuterRadius2 = OUTER_RADIUS*OUTER_RADIUS;
+static const float kInnerRadius = 1.0;
+static const float kInnerRadius2 = 1.0;
 
-static const half kCameraHeight = 0.0001;
+static const float kCameraHeight = 0.0001;
 
 #define kMIE 0.0010             // Mie constant
 #define kSUN_BRIGHTNESS 20.0    // Sun brightness
 
 #define kMAX_SCATTER 50.0 // Maximum scattering value, to prevent math overflows on Adrenos
 
-static const half kHDSundiskIntensityFactor = 15.0;
-static const half kSimpleSundiskIntensityFactor = 27.0;
+static const float kHDSundiskIntensityFactor = 15.0;
+static const float kSimpleSundiskIntensityFactor = 27.0;
 
-static const half kSunScale = 400.0 * kSUN_BRIGHTNESS;
-static const half kKmESun = kMIE * kSUN_BRIGHTNESS;
-static const half kKm4PI = kMIE * 4.0 * 3.14159265;
-static const half kScale = 1.0 / (OUTER_RADIUS - 1.0);
-static const half kScaleDepth = 0.25;
-static const half kScaleOverScaleDepth = (1.0 / (OUTER_RADIUS - 1.0)) / 0.25;
-static const half kSamples = 2.0; // THIS IS UNROLLED MANUALLY, DON'T TOUCH
+static const float kSunScale = 400.0 * kSUN_BRIGHTNESS;
+static const float kKmESun = kMIE * kSUN_BRIGHTNESS;
+static const float kKm4PI = kMIE * 4.0 * 3.14159265;
+static const float kScale = 1.0 / (OUTER_RADIUS - 1.0);
+static const float kScaleDepth = 0.25;
+static const float kScaleOverScaleDepth = (1.0 / (OUTER_RADIUS - 1.0)) / 0.25;
+static const float kSamples = 2.0; // THIS IS UNROLLED MANUALLY, DON'T TOUCH
 
 #define MIE_G (-0.990)
 #define MIE_G2 0.9801
@@ -53,62 +53,62 @@ static const half kSamples = 2.0; // THIS IS UNROLLED MANUALLY, DON'T TOUCH
 
 #define SKY_GROUND_THRESHOLD 0.02
 
-half scale(float inCos)
+float scale(float inCos)
 {
-    half x = 1.0 - inCos;
+    float x = 1.0 - inCos;
     return 0.25 * exp(-0.00287 + x*(0.459 + x*(3.83 + x*(-6.80 + x*5.25))));
 }
 
-half getRayleighPhase(half eyeCos2)
+float getRayleighPhase(float eyeCos2)
 {
     return 0.75 + 0.75 * eyeCos2;
 }
 
-half getRayleighPhase(half3 light, half3 ray)
+float getRayleighPhase(float3 light, float3 ray)
 {
-    half eyeCos = dot(light, ray);
+    float eyeCos = dot(light, ray);
     return getRayleighPhase(eyeCos * eyeCos);
 }
 
-half3 RotateAroundYInDegrees (half3 vertex, half degrees)
+float3 RotateAroundYInDegrees (float3 vertex, float degrees)
 {
-    half alpha = degrees * SKYBOX_PI / 180.0;
-    half sina, cosa;
+    float alpha = degrees * SKYBOX_PI / 180.0;
+    float sina, cosa;
     sincos(alpha, sina, cosa);
-    half2x2 m = half2x2(cosa, -sina, sina, cosa);
-    return half3(mul(m, vertex.xz), vertex.y).xzy;
+    float2x2 m = float2x2(cosa, -sina, sina, cosa);
+    return float3(mul(m, vertex.xz), vertex.y).xzy;
 }
 
 // Calculates the Mie phase function
-half getMiePhase(half eyeCos, half eyeCos2, half size)
+float getMiePhase(float eyeCos, float eyeCos2, float size)
 {
-    half temp = 1.0 + MIE_G2 - 2.0 * MIE_G * eyeCos;
+    float temp = 1.0 + MIE_G2 - 2.0 * MIE_G * eyeCos;
     temp = pow(temp, pow(size,0.65) * 10);
-    temp = max(temp,1.0e-4); // prevent division by zero, esp. in half precision
+    temp = max(temp,1.0e-4); // prevent division by zero, esp. in float precision
     temp = 1.5 * ((1.0 - MIE_G2) / (2.0 + MIE_G2)) * (1.0 + eyeCos2) / temp;
     return temp;
 }
 
 // Calculates the sun shape
-half calcSunAttenuation(half3 lightPos, half3 ray, half size, half sizeCoverage)
+float calcSunAttenuation(float3 lightPos, float3 ray, float size, float sizeCoverage)
 {
-    half focusedEyeCos = pow(saturate(dot(lightPos, ray)), sizeCoverage);
+    float focusedEyeCos = pow(saturate(dot(lightPos, ray)), sizeCoverage);
     return getMiePhase(-focusedEyeCos, focusedEyeCos * focusedEyeCos, size);
 }
 
 
-half2x3 CalculateSkyboxVert(half3 eyeRay, half3 direction, half rayLength, half3 skyTint)
+float2x3 CalculateSkyboxVert(float3 eyeRay, float3 direction, float rayLength, float3 skyTint)
 {
-    half3 kScatteringWavelength = lerp (
+    float3 kScatteringWavelength = lerp (
     kDefaultScatteringWavelength - kVariableRangeForScatteringWavelength,
     kDefaultScatteringWavelength + kVariableRangeForScatteringWavelength,
-    half3(1, 1, 1) - skyTint); // using Tint in sRGB gamma allows for more visually linear interpolation and to keep (.5) at (128, gray in sRGB) point
-    half kKrESun = rayLength * kSUN_BRIGHTNESS;
-    half kKr4PI = rayLength * 4.0 * 3.14159265;
-    half3 kInvWavelength = 1.0 / pow(kScatteringWavelength, 4);
-    half3 cameraPos = half3(0,kInnerRadius + kCameraHeight,0);    // The camera's current position
-    half far = 0.0;
-    half3 cIn, cOut;
+    float3(1, 1, 1) - skyTint); // using Tint in sRGB gamma allows for more visually linear interpolation and to keep (.5) at (128, gray in sRGB) point
+    float kKrESun = rayLength * kSUN_BRIGHTNESS;
+    float kKr4PI = rayLength * 4.0 * 3.14159265;
+    float3 kInvWavelength = 1.0 / pow(kScatteringWavelength, 4);
+    float3 cameraPos = float3(0,kInnerRadius + kCameraHeight,0);    // The camera's current position
+    float far = 0.0;
+    float3 cIn, cOut;
 
     if (eyeRay.y >= -0.1)
     {
@@ -116,44 +116,44 @@ half2x3 CalculateSkyboxVert(half3 eyeRay, half3 direction, half rayLength, half3
         // Calculate the length of the "atmosphere"
         far = sqrt(kOuterRadius2 + kInnerRadius2 * eyeRay.y * eyeRay.y - kInnerRadius2) - kInnerRadius * eyeRay.y;
 
-        half3 pos = cameraPos + far * eyeRay;
+        float3 pos = cameraPos + far * eyeRay;
 
         // Calculate the ray's starting position, then calculate its scattering offset
-        half height = kInnerRadius + kCameraHeight;
-        half depth = exp(kScaleOverScaleDepth * (-kCameraHeight));
-        half startAngle = dot(eyeRay, cameraPos) / height;
-        half startOffset = depth*scale(startAngle);
+        float height = kInnerRadius + kCameraHeight;
+        float depth = exp(kScaleOverScaleDepth * (-kCameraHeight));
+        float startAngle = dot(eyeRay, cameraPos) / height;
+        float startOffset = depth*scale(startAngle);
 
         // Initialize the scattering loop variables
-        half sampleLength = far / kSamples;
-        half scaledLength = sampleLength * kScale;
-        half3 sampleRay = eyeRay * sampleLength;
-        half3 samplePoint = cameraPos + sampleRay * 0.5;
+        float sampleLength = far / kSamples;
+        float scaledLength = sampleLength * kScale;
+        float3 sampleRay = eyeRay * sampleLength;
+        float3 samplePoint = cameraPos + sampleRay * 0.5;
 
         // Now loop through the sample rays
-        half3 frontColor = half3(0.0, 0.0, 0.0);
+        float3 frontColor = float3(0.0, 0.0, 0.0);
         // Weird workaround: WP8 and desktop FL_9_3 do not like the for loop here
         // (but an almost identical loop is perfectly fine in the ground calculations below)
         // Just unrolling this manually seems to make everything fine again.
         //              for(int i=0; i<int(kSamples); i++)
         {
-            half height = length(samplePoint);
-            half depth = exp(kScaleOverScaleDepth * (kInnerRadius - height));
-            half lightAngle = dot(direction.xyz, samplePoint) / height;
-            half cameraAngle = dot(eyeRay, samplePoint) / height;
-            half scatter = (startOffset + depth*(scale(lightAngle) - scale(cameraAngle)));
-            half3 attenuate = exp(-clamp(scatter, 0.0, kMAX_SCATTER) * (kInvWavelength * kKr4PI + kKm4PI));
+            float height = length(samplePoint);
+            float depth = exp(kScaleOverScaleDepth * (kInnerRadius - height));
+            float lightAngle = dot(direction.xyz, samplePoint) / height;
+            float cameraAngle = dot(eyeRay, samplePoint) / height;
+            float scatter = (startOffset + depth*(scale(lightAngle) - scale(cameraAngle)));
+            float3 attenuate = exp(-clamp(scatter, 0.0, kMAX_SCATTER) * (kInvWavelength * kKr4PI + kKm4PI));
 
             frontColor += attenuate * (depth * scaledLength);
             samplePoint += sampleRay;
         }
         {
-            half height = length(samplePoint);
-            half depth = exp(kScaleOverScaleDepth * (kInnerRadius - height));
-            half lightAngle = dot(direction.xyz, samplePoint) / height;
-            half cameraAngle = dot(eyeRay, samplePoint) / height;
-            half scatter = (startOffset + depth*(scale(lightAngle) - scale(cameraAngle)));
-            half3 attenuate = exp(-clamp(scatter, 0.0, kMAX_SCATTER) * (kInvWavelength * kKr4PI + kKm4PI));
+            float height = length(samplePoint);
+            float depth = exp(kScaleOverScaleDepth * (kInnerRadius - height));
+            float lightAngle = dot(direction.xyz, samplePoint) / height;
+            float cameraAngle = dot(eyeRay, samplePoint) / height;
+            float scatter = (startOffset + depth*(scale(lightAngle) - scale(cameraAngle)));
+            float3 attenuate = exp(-clamp(scatter, 0.0, kMAX_SCATTER) * (kInvWavelength * kKr4PI + kKm4PI));
 
             frontColor += attenuate * (depth * scaledLength);
             samplePoint += sampleRay;
@@ -167,27 +167,27 @@ half2x3 CalculateSkyboxVert(half3 eyeRay, half3 direction, half rayLength, half3
         // Ground
         far = (-kCameraHeight) / (min(-0.001, eyeRay.y));
 
-        half3 pos = cameraPos + far * eyeRay;
+        float3 pos = cameraPos + far * eyeRay;
 
         // Calculate the ray's starting position, then calculate its scattering offset
-        half cameraAngle = dot(-eyeRay, pos);
-        half lightAngle = dot(_SunDirection.xyz, pos);
-        half cameraScale = scale(cameraAngle);
-        half lightScale = scale(lightAngle);
-        // half depth = exp((-kCameraHeight) * (1.0/kScaleDepth));
-        // half cameraOffset = depth * cameraScale;
-        half cameraOffset = exp((-kCameraHeight) * (1.0/kScaleDepth)) * cameraScale;
-        half temp = (lightScale + cameraScale);
+        float cameraAngle = dot(-eyeRay, pos);
+        float lightAngle = dot(_SunDirection.xyz, pos);
+        float cameraScale = scale(cameraAngle);
+        float lightScale = scale(lightAngle);
+        // float depth = exp((-kCameraHeight) * (1.0/kScaleDepth));
+        // float cameraOffset = depth * cameraScale;
+        float cameraOffset = exp((-kCameraHeight) * (1.0/kScaleDepth)) * cameraScale;
+        float temp = (lightScale + cameraScale);
 
         // Initialize the scattering loop variables
-        half sampleLength = far / kSamples;
-        half scaledLength = sampleLength * kScale;
-        half3 sampleRay = eyeRay * sampleLength;
-        half3 samplePoint = cameraPos + sampleRay * 0.5;
+        float sampleLength = far / kSamples;
+        float scaledLength = sampleLength * kScale;
+        float3 sampleRay = eyeRay * sampleLength;
+        float3 samplePoint = cameraPos + sampleRay * 0.5;
 
         // Now loop through the sample rays
-        half3 frontColor = half3(0.0, 0.0, 0.0);
-        half3 attenuate;
+        float3 frontColor = float3(0.0, 0.0, 0.0);
+        float3 attenuate;
         {
             float height = length(samplePoint);
             float depth = exp(kScaleOverScaleDepth * (kInnerRadius - height));
@@ -200,8 +200,60 @@ half2x3 CalculateSkyboxVert(half3 eyeRay, half3 direction, half rayLength, half3
         cIn = frontColor * (kInvWavelength * kKrESun + kKmESun);
         cOut = clamp(attenuate, 0.0, 1.0);
     }
-    return half2x3(cIn, cOut);
+    return float2x3(cIn, cOut);
 }
+
+
+
+
+//// float ////
+void SkyboxVert_half(in float3 vertexIn, in float3 skyTint, in float exposure,
+                    in float3 groundColorIn, in float3 sunDirection, in float3 sunColorIn, in float atmosphereThickness,
+                    out float3 vertexOut, out float3 groundColorOut, out float3 skyColor, out float3 sunColorOut)
+{
+    float rayLength = lerp(0.0, 0.0025, pow(atmosphereThickness, 2.5));
+
+    // Get the ray from the camera to the vertex and its length (which is the far point of the ray passing through the atmosphere)
+    float3 eyeRay = normalize(mul((float3x3)unity_ObjectToWorld, vertexIn.xyz));
+
+    vertexOut = -eyeRay;
+
+    // if we want to calculate color in vprog:
+    // 1. in case of linear: multiply by _Exposure in here (even in case of lerp it will be common multiplier, so we can skip mul in fshader)
+    // 2. in case of gamma and SKYBOX_COLOR_IN_TARGET_COLOR_SPACE: do sqrt right away instead of doing that in fshader
+
+    float2x3 color = CalculateSkyboxVert(eyeRay, sunDirection, rayLength, _SkyTint);
+    
+    groundColorOut = exposure * (color[0] + groundColorIn * color[1]);
+    skyColor = exposure * (color[0] * getRayleighPhase(sunDirection.xyz, -eyeRay));
+    float lightColorIntensity = clamp(length(sunColorIn.xyz), 0.25, 1);
+    sunColorOut = kHDSundiskIntensityFactor * saturate(color[1]) * sunColorIn.xyz / lightColorIntensity;
+}
+
+
+void GetRay_half(in float3 vertex, out float3 ray, out float yValue)
+{
+    ray = normalize(vertex.xyz);
+    yValue = ray.y / SKY_GROUND_THRESHOLD;
+}
+
+
+void GetSkyColor_half(in float yValue, in float3 groundColor, in float3 skyColor, out float3 col)
+{
+    col = lerp(skyColor, groundColor, saturate(yValue));
+}
+
+
+void GetSunColor_half(in float3 ray, in float yValue, in float3 direction, in float size, in float coverage, in float3 sunColor, out float3 result)
+{
+    if (yValue < -0.1)
+    {
+        result = sunColor * calcSunAttenuation(direction.xyz, -ray, size, coverage);
+    }
+    else result = float3(0.0, 0.0, 0.0);
+}
+
+
 
 
 
@@ -209,14 +261,14 @@ half2x3 CalculateSkyboxVert(half3 eyeRay, half3 direction, half rayLength, half3
 
 
 //// float ////
-void SkyboxVert_float(in half3 vertexIn, in half3 skyTint, in half exposure,
-                    in half3 groundColorIn, in half3 sunDirection, in half3 sunColorIn, in half atmosphereThickness,
-                    out half3 vertexOut, out half3 groundColorOut, out half3 skyColor, out half3 sunColorOut)
+void SkyboxVert_float(in float3 vertexIn, in float3 skyTint, in float exposure,
+                    in float3 groundColorIn, in float3 sunDirection, in float3 sunColorIn, in float atmosphereThickness,
+                    out float3 vertexOut, out float3 groundColorOut, out float3 skyColor, out float3 sunColorOut)
 {
-    half rayLength = lerp(0.0, 0.0025, pow(atmosphereThickness, 2.5));
+    float rayLength = lerp(0.0, 0.0025, pow(atmosphereThickness, 2.5));
 
     // Get the ray from the camera to the vertex and its length (which is the far point of the ray passing through the atmosphere)
-    half3 eyeRay = normalize(mul((half3x3)unity_ObjectToWorld, vertexIn.xyz));
+    float3 eyeRay = normalize(mul((float3x3)unity_ObjectToWorld, vertexIn.xyz));
 
     vertexOut = -eyeRay;
 
@@ -224,88 +276,33 @@ void SkyboxVert_float(in half3 vertexIn, in half3 skyTint, in half exposure,
     // 1. in case of linear: multiply by _Exposure in here (even in case of lerp it will be common multiplier, so we can skip mul in fshader)
     // 2. in case of gamma and SKYBOX_COLOR_IN_TARGET_COLOR_SPACE: do sqrt right away instead of doing that in fshader
 
-    half2x3 color = CalculateSkyboxVert(eyeRay, sunDirection, rayLength, _SkyTint);
+    float2x3 color = CalculateSkyboxVert(eyeRay, sunDirection, rayLength, _SkyTint);
     
     groundColorOut = exposure * (color[0] + groundColorIn * color[1]);
     skyColor = exposure * (color[0] * getRayleighPhase(sunDirection.xyz, -eyeRay));
-    half lightColorIntensity = clamp(length(sunColorIn.xyz), 0.25, 1);
+    float lightColorIntensity = clamp(length(sunColorIn.xyz), 0.25, 1);
     sunColorOut = kHDSundiskIntensityFactor * saturate(color[1]) * sunColorIn.xyz / lightColorIntensity;
 }
 
 
-void GetRay_float(in half3 vertex, out half3 ray, out half yValue)
+void GetRay_float(in float3 vertex, out float3 ray, out float yValue)
 {
     ray = normalize(vertex.xyz);
     yValue = ray.y / SKY_GROUND_THRESHOLD;
 }
 
 
-void GetSkyColor_float(in half yValue, in half3 groundColor, in half3 skyColor, out half3 col)
+void GetSkyColor_float(in float yValue, in float3 groundColor, in float3 skyColor, out float3 col)
 {
     col = lerp(skyColor, groundColor, saturate(yValue));
 }
 
 
-void GetSunColor_float(in half3 ray, in half yValue, in half3 direction, in half size, in half coverage, in half3 sunColor, out half3 result)
+void GetSunColor_float(in float3 ray, in float yValue, in float3 direction, in float size, in float coverage, in float3 sunColor, out float3 result)
 {
     if (yValue < -0.1)
     {
         result = sunColor * calcSunAttenuation(direction.xyz, -ray, size, coverage);
     }
-    else result = half3(0.0, 0.0, 0.0);
-}
-
-
-
-
-
-
-
-
-
-//// half ////
-void SkyboxVert_half(in half3 vertexIn, in half3 skyTint, in half exposure,
-                    in half3 groundColorIn, in half3 sunDirection, in half3 sunColorIn, in half atmosphereThickness,
-                    out half3 vertexOut, out half3 groundColorOut, out half3 skyColor, out half3 sunColorOut)
-{
-    half rayLength = lerp(0.0, 0.0025, pow(atmosphereThickness, 2.5));
-
-    // Get the ray from the camera to the vertex and its length (which is the far point of the ray passing through the atmosphere)
-    half3 eyeRay = normalize(mul((half3x3)unity_ObjectToWorld, vertexIn.xyz));
-
-    vertexOut = -eyeRay;
-
-    // if we want to calculate color in vprog:
-    // 1. in case of linear: multiply by _Exposure in here (even in case of lerp it will be common multiplier, so we can skip mul in fshader)
-    // 2. in case of gamma and SKYBOX_COLOR_IN_TARGET_COLOR_SPACE: do sqrt right away instead of doing that in fshader
-
-    half2x3 color = CalculateSkyboxVert(eyeRay, sunDirection, rayLength, _SkyTint);
-    
-    groundColorOut = exposure * (color[0] + groundColorIn * color[1]);
-    skyColor = exposure * (color[0] * getRayleighPhase(sunDirection.xyz, -eyeRay));
-    half lightColorIntensity = clamp(length(sunColorIn.xyz), 0.25, 1);
-    sunColorOut = kHDSundiskIntensityFactor * saturate(color[1]) * sunColorIn.xyz / lightColorIntensity;
-}
-
-
-void GetRay_half(in half3 vertex, out half3 ray, out half yValue)
-{
-    ray = normalize(vertex.xyz);
-    yValue = ray.y / SKY_GROUND_THRESHOLD;
-}
-
-
-void GetSkyColor_half(in half yValue, in half3 groundColor, in half3 skyColor, out half3 col)
-{
-    col = lerp(skyColor, groundColor, saturate(yValue));
-}
-
-
-void GetSunColor_half(in half3 ray, in half yValue, in half3 direction, in half size, in half coverage, in half3 sunColor, out half3 result)
-{
-    if (yValue < -0.1)
-    {
-        result = sunColor * calcSunAttenuation(direction.xyz, -ray, size, coverage);
-    }
-    else result = half3(0.0, 0.0, 0.0);
+    else result = float3(0.0, 0.0, 0.0);
 }
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # URP-Advanced-Procedural-Skybox
 Advanced procedural skybox that include clouds, stars, sun and moon working in Universal Render Pipeline.
+
+- Moon Texture
+https://nasa3d.arc.nasa.gov/detail/as11-44-6665
+
+- Stars Cubemap
+https://tools.wwwtyro.net/space-3d/index.html
+
+- Cloud Mask Texture
+From com.unity.render-pipelines.high-definition package
